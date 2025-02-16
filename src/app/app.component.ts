1ddd7043483a91fea04b0d81a950f50599cc7436
Juan Reyes <camiloreyes1125@gmail.com>
2025-02-16 15:28:36 -0500

    rendering data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { WeaponService } from './weapon.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'msic-crud';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'type']; // Table columns to display
+  dataSource!: MatTableDataSource<any>; // Data source for the Material table
+
+  constructor(
+    private dialog: MatDialog, // To manage dialogs for add/edit forms
+    private weaponService: WeaponService // Service to fetch weapon data
+  ) {}
+
+  ngOnInit(): void {
+    this.getWeapons(); // Fetch weapons data when the component initializes
+  }
+
+  // Method to fetch weapons data from the service
+  getWeapons(): void {
+    this.weaponService.getWeapons().subscribe(
+      (weapons) => {
+        this.dataSource = new MatTableDataSource(weapons); // Assign data to the table
+      },
+      (error) => {
+        console.error('Failed to fetch weapons data', error); // Handle errors gracefully
+      }
+    );
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WeaponAddEditComponent } from './weapon-add-edit/weapon-add-edit.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card'
-
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +19,10 @@ import { MatCardModule } from '@angular/material/card'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule,
+    MatDialogModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
